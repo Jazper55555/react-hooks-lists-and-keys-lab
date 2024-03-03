426058c3944cc1dfd520461677aa679d05020e1f
Jazper <jazpersaldana@yahoo.com>
2024-03-03 14:50:21 -0600

    list and keys lab complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  
+  const linkNames = links.map((link) => {
+    return <a key={link} href={`#${link}`}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {linkNames}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  let techId = 1
+
+  const technologyNames = technologies.map((tech) => {
+    return <span key={techId++}>{tech}</span>
+  })
+
+  console.log(technologyNames)
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologyNames}
       </div>
     </div>
   );
